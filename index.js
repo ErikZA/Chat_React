56c65941e9c0295e69e898a908d5c43eafeaa6a2
ErikZambeli <erik_12_nf@live.com>
2018-12-06 03:15:27 -0200

    final

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const path = require('path');
 var app = require('./app');
 var express = require('express');
 const appSet = express();
+const bodyParser = require('body-parser');
 // Serve static files from the React app
-appSet.use(express.static(path.join(__dirname, 'client/build')));
 
-
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-appSet.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+appSet.use(bodyParser.json());
+appSet.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
 app.set('port', port);
